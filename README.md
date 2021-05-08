805ade6877e9addd69c112c6d3cbe7a089e480a9
Aseel-Z <aseelalzweri@gmail.com>
2021-05-09 02:42:51 +0300

    version2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Live(Deployed) URL:
-Open PR: https://github.com/Aseel-Z/snakes-cafe/pull/1
+Open PR: https://github.com/Aseel-Z/snakes-cafe/pull/2
--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,11 +1,10 @@
 # When run, the program should print an intro message and the menu for the restaurant
 
-menue = ["
-   {"Appetizers": ["Wings", "Cookies", "Spring Rolls"]}, 
-   {"Entrees": ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"]}, 
-  {"Desserts": ["Ice Cream", "Cake", "Pie"]}, 
-{"Drinks": ["Coffee", "Tea", "Unicorn Tears"]},
-]
+menue = {"Appetizers": ["Wings", "Cookies", "Spring Rolls"],
+   "Entrees": ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"], 
+ "Desserts": ["Ice Cream", "Cake", "Pie"], 
+"Drinks": ["Coffee", "Tea", "Unicorn Tears"],
+}
 
 def printMenueIntro():
     print('**************************************')
@@ -13,32 +12,47 @@ def printMenueIntro():
     print('**    Please see our menu below.    **')
     print('**')
     print('** To quit at any time, type "quit" **')
-    print('**************************************')
-    for category in menue:
-        for key, value in menue[category]:
-            print(key)
-            print('----------')
-            for dish in value:
-                print(dish)
-    
-    
+    print('**************************************\n')
+    print('')
+    categories = menue.items()
+    print(categories)
+    for key, value in categories:
+      print(key)
+      print('-'*len(key))
+      print(value)
+      for dish in value:
+        print(dish)
+      print('\n')
+
 
 def enterOrder():
+    count=0
+    count_2=0
     print('***********************************')
     print('** What would you like to order? **')
-    print('***********************************')
-    order=input('Enter Your Favorite Dish')
-    count=0
-    for category in menue:
-        for key, value in menue[category]:
-            for dish in value:
-                if order==dish:
-                    count+=1
-    print('** {count} order of {order} have been added to your meal **')
-
+    print('***********************************\n')
+    order = input('> ')
+    print('\n')
+    dish = order
+    while order:
+      if order == 'quit':
+        exitProgram()
+      else:
+        if dish == order:
+            count+=1
+            print(f'** {count} order of {order} have been added to your meal **\n')
+        else:
+            dish_2=order
+            count_2+=1
+            print(f'** {count_2} order of {order} have been added to your meal **\n')
+        order=''
+        order = input('> ')
+        print('\n')
+          
+          
 def exitProgram():
-    exitInput=input('Would you like to exit the program?(y/n)')
-    if exitInput='y':
+    exitInput=input('Would you like to exit the program?quit(y/n) \n> ')
+    if exitInput=='y':
         exit()
     else:
         enterOrder()
@@ -46,6 +60,3 @@ def exitProgram():
 
 printMenueIntro()
 enterOrder()
-exitProgram()
-
-
